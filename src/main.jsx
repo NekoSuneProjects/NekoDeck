1ed87f917d36f1abf9f2c194ecade275996c7705
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-13 08:36:41 +0100

    feat: enable Unity and full external Activity Studio

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppV2 from './AppV2.jsx';
-import ActivityStudio from './ActivityStudio.jsx';
+import ActivityStudioV2 from './ActivityStudioV2.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppV2 />
-    <ActivityStudio />
+    <ActivityStudioV2 />
   </React.StrictMode>
 );
