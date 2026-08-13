4235f687188bde782b08031ffe977b79b260f80b
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-13 08:47:19 +0100

    feat: wire Activity management before web host routes

--- server/v3.cjs
+++ server/v3.cjs
@@ -1,11 +1,13 @@
 const { createApp: createBaseApp } = require('./app.cjs');
 const { registerV2 } = require('./v2.cjs');
+const { registerActivityManagementRoutes } = require('./activity-management.cjs');
 const { registerAdvancedActivityRoutes } = require('./activity-web-host.cjs');
 const { registerActivityRoutes } = require('./activities.cjs');
 
 function createApp(options = {}) {
   const out = createBaseApp(options);
   registerV2(out.app, out.store);
+  registerActivityManagementRoutes(out.app, out.store, options);
   registerAdvancedActivityRoutes(out.app, out.store, options);
   registerActivityRoutes(out.app, out.store, options);
   return out;
