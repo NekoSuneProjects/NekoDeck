2e981a14998e4223bcc1c6d2d129679f322b6bb1
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-13 08:03:58 +0100

    feat: mount Activity Studio alongside Profile Widgets

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppV2 from './AppV2.jsx';
+import ActivityStudio from './ActivityStudio.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode><AppV2 /></React.StrictMode>
+  <React.StrictMode>
+    <AppV2 />
+    <ActivityStudio />
+  </React.StrictMode>
 );
