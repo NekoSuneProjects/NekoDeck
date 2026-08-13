7fcdcfe572e00e87c959bd84c033db7519008eae
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-13 08:51:18 +0100

    feat: mount Activity Studio 0.4 and Profile Widget manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppV2 from './AppV2.jsx';
-import ActivityStudioV2 from './ActivityStudioV2.jsx';
+import ActivityStudioV3 from './ActivityStudioV3.jsx';
+import ProfileWidgetManager from './ProfileWidgetManager.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppV2 />
-    <ActivityStudioV2 />
+    <ActivityStudioV3 />
+    <ProfileWidgetManager />
   </React.StrictMode>
 );
